ba471890e2468d53d2bbc0db0b9f5ce402ad5b86
Mario R <48869294+mario-r99@users.noreply.github.com>
2024-01-29 19:41:44 +0000

    Fix path name

--- fix.sh
+++ fix.sh
@@ -1,4 +1,4 @@
 #! /bin/sh
 mount /dev/nvme0n1p12 /mnt
-sed -i 's/cros_efi/cros_efi cros_debug usbcore.autosuspend=-1/g' /mnt/boot/efi/grub.cfg
+sed -i 's/cros_efi/cros_efi cros_debug usbcore.autosuspend=-1/g' /mnt/efi/boot/grub.cfg
 umount /mnt
